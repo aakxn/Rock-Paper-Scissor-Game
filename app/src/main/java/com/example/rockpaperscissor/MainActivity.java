762ac580b032e9a2245b3c4cbd3d80e513ef86d4
Gauri Sonawane <gaurisonawane1105@gmail.com>
2024-04-10 22:24:41 +0530

    The base that we made together in clg

---
+++ app/src/main/java/com/example/rockpaperscissor/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.rockpaperscissor;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
+
